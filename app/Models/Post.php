9924172a4812be5a1f853fd3fc5c96ed708ed1f4
puspakirana <puspakiranastv@gmail.com>
2022-08-09 14:47:44 +0700

    Lesson 17 - Dashboard UI

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -52,4 +52,9 @@ class Post extends Model
             )
         );
     }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,28 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+    <div class="row my-3">
+        <div class="col-lg-8">
+            <h2>{{ $post->title }}</h2>
+            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
+            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+
+            <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3"/>
+
+
+            {{-- <h5>{{ $post->author }}</h5> --}}
+            {{-- {{  }} -> untuk escape html tag
+            {!!  !!} -> tidak escape html tag --}}
+
+            <article class="my-3 fs-5 ">
+                {!! $post->body !!}
+            </article>
+
+{{--
+            <a href="/posts" class="d-block mt-3">Back to Posts</a> --}}
+        </div>
+    </div>
+</div>
+@endsection
