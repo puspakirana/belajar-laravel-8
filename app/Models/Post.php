291767adfdc9846182d47490d621ac8c151a9ce9
puspakirana <puspakiranastv@gmail.com>
2022-08-10 23:43:04 +0700

    Lesson 18 - Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
 
     //boleh diisi
     // protected $fillable = ['title', 'excerpt', 'body'];
--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -7,6 +7,15 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="/css/dashboard.css" rel="stylesheet">
+        {{-- Trix Editor --}}
+        <link rel="stylesheet" type="text/css" href="/css/trix.css">
+        <script type="text/javascript" src="/js/trix.js"></script>
+
+        <style>
+            trix-toolbar [data-trix-button-group="file-tools"]{
+                display: none;
+            }
+        </style>
     </head>
     <body>
 
