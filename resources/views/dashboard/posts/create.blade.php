a0335128ff44a1744450fedf3eb3b09ff088f96f
puspakirana <puspakiranastv@gmail.com>
2022-08-16 16:37:50 +0700

    Lesson 19 - Validation & Insert Post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -7,27 +7,48 @@
 </div>
 
 <div class="col-lg-8">
-    <form method="POST" action="/dashboard/posts">
+    <form method="POST" action="/dashboard/posts" class="mb-5">
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" name="title" class="form-control" id="title">
+            <input type="text" name="title" class="form-control @error('title')
+                is-invalid
+            @enderror" id="title" required autofocus value="{{ old('title') }}">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-            <input type="text" name="slug" class="form-control" id="slug" disabled readonly>
+            <input type="text" name="slug" class="form-control @error('slug')
+                is-invalid
+            @enderror" id="slug" required value="{{ old('slug') }}">
+            @error('slug')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="category" class="form-label">Category</label>
             <select class="form-select" name="category_id">
                 @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @if(old('category') == $category->id)
+                        <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                    @else
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
                 @endforeach
             </select>
         </div>
         <div class="mb-3">
             <label for="body" class="form-label">Body</label>
-            <input id="body" type="hidden" name="body">
+            @error('body')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
+            <input id="body" type="hidden" name="body" value="{{ old('body') }}">
             <trix-editor input="body"></trix-editor>
         </div>
 
@@ -46,10 +67,11 @@
             .then(data => slug.value = data.slug)
     });
 
-    document.addEventListener('trix-file-accept' function(e){
+    document.addEventListener('trix-file-accept', function(e){
         e.preventDefault();
-    })
+    });
 
 </script>
 
 @endsection
+
--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -7,6 +7,13 @@
 
     <div class="table-responsive col-lg-8">
         <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create New Post</a>
+
+        @if (session()->has('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+
         <table class="table table-striped table-sm">
             <thead>
                 <tr>
